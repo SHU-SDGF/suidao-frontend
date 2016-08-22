f6127398c8ae12db7212b79a3b4d85e56555e9c2
jeremy_shao <jeremy.shao@pwc.com>
2016-08-22 23:51:21 +0800

    add export

--- app/providers/index.ts
+++ app/providers/index.ts
@@ -1,2 +1,4 @@
 export {UserService} from './user_service';
 export {LookupService} from './lookup_service.ts';
+export {EnvironmentActivitySummary} from './environment_activity_summary.ts';
+export {EnvironmentActivity} from './environment_activity.ts';
