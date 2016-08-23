c92e9dbb88f3eabc2dd71a5106ce90a3f3f174d8
jeremy_shao <jeremy.shao@pwc.com>
2016-08-23 22:29:10 +0800

    add two interfaces

--- app/providers/environment_activity_service.ts
+++ app/providers/environment_activity_service.ts
@@ -4,6 +4,37 @@ import { Storage, LocalStorage } from 'ionic-angular';
 import { AppConfig } from './config';
 import {URLSearchParams} from '@angular/http';
 
+export interface EnvironmentActivity {
+  act_no: string //活动编码
+	insp_date: any //巡检日期
+	end_date: any //更新日时
+	act_status: any //活动状态 
+	act_type: any //活动类型
+	description: string // 描述
+	create_user: string //作成者
+	update_user: string // 更新者
+	photo: any // 图片
+	audio: any // 音频
+	video: any // 视频
+	recorder: string //记录人
+	create_date: any //作成日时
+	update_date: any //更新日时
+}
+
+export interface EnvironmentActivitySummary {
+	act_no: string //活动编码
+	act_name: string //活动名称
+	start_date: any //起始日期
+	end_date: any //结束日期
+	description: string //活动描述
+	longitude: number //经度
+	latitude: number //纬度
+	create_user: string //作成者
+	update_user: string //更新者
+	create_date: any //作成日时
+	update_date: any //更新日时
+}
+
 @Injectable()
 export class EnvironmentActivityService {
 	constructor(public http: Http) {}
