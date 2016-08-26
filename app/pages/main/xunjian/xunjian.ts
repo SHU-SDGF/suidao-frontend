b82030e4f3cb7a58a45794a9130812ab16017c3c
GeorgeLin <georgelin@GeorgeLindeiMac.local>
2016-08-27 02:38:03 +0800

    save

--- app/pages/main/xunjian/xunjian.ts
+++ app/pages/main/xunjian/xunjian.ts
@@ -12,12 +12,16 @@ import {UndergroundPage} from './underground/underground';
 })
 export class XunjianPage extends ToggleMenu{
   private onGround: boolean = true;
+  private _pageTitles: Array<string> = ['环境巡检', '隧道巡检'];
+  private _pageTitle: string;
 
   constructor(private _menuCtrl: MenuController){
     super(_menuCtrl);
+    this._pageTitle = this._pageTitles[0];
   }
 
   private switchView(){
+    this._pageTitle = this._pageTitles[<any>this['onGround'] | 0];
     this.onGround = !this.onGround;
   }
 }
