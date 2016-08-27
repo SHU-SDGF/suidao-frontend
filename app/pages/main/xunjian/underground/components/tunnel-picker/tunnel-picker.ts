5382047abe2159a8eccf0bbdeb0296b214a71763
ytyszxf <ljz135790@gmail.com>
2016-08-27 14:19:04 +0800

    save

---
+++ app/pages/main/xunjian/underground/components/tunnel-picker/tunnel-picker.ts
@@ -0,0 +1,13 @@
+import {Component, OnInit, ViewChild, EventEmitter} from '@angular/core';
+import {Events,
+  ViewController
+} from 'ionic-angular';
+
+@Component({
+  templateUrl: './build/pages/main/xunjian/underground/components/tunnerl-picker/tunnerl-picker.html',
+})
+export class TunnelPicker implements OnInit{
+  ngOnInit() {
+    
+  }
+}
