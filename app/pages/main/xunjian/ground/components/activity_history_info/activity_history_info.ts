4df5dfbbaaf58a7496f0d3ba14b8fed6f3a7f6bf
jeremy_shao <jeremy.shao@pwc.com>
2016-09-01 23:42:16 +0800

    fix some issues

--- app/pages/main/xunjian/ground/components/activity_history_info/activity_history_info.ts
+++ app/pages/main/xunjian/ground/components/activity_history_info/activity_history_info.ts
@@ -41,8 +41,8 @@ export class ActivityHistoryInfoPage implements OnInit{
     let activityParams = this.params.get('activityDetail');
     this.activityDetailObj = {
       actName: activityName,
-      inspDate: activityParams["inspDate"],
-      endDate: activityParams["inspDate"],
+      inspDate: this._convertDate(activityParams["inspDate"]),
+      endDate: activityParams["endDate"],
       description: activityParams["description"], //活动描述
       actStatus: activityParams["actStatus"],
       recorder: activityParams["recorder"],
@@ -51,6 +51,11 @@ export class ActivityHistoryInfoPage implements OnInit{
       video: activityParams["video"],
       actNo: activityParams["actNo"]
     };
+  }
+
+  private _convertDate(datetime) {
+    let date = new Date(datetime)
+    return date.getFullYear() + '-0' + (date.getMonth() + 1) + '-0' + (date.getDay() + 1)
   }
 
   private _getLookUpValue(list, order){
