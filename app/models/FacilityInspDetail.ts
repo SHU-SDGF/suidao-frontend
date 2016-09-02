89c708a81b9e51639b85301816fbdf2436e5a1ba
jeremy_shao <jeremy.shao@pwc.com>
2016-09-02 21:29:29 +0800

    add two classes for underground

---
+++ app/models/FacilityInspDetail.ts
@@ -0,0 +1,44 @@
+import {JsonProperty, Serializable} from '../providers/JsonMapper';
+import {_baseClass} from './_baseClass';
+
+@Serializable()
+export class FacilityInspDetail extends _baseClass { // 结构巡检明细表
+	id: string //主键id
+	area: any //面积，
+	delFlg: any //删除标记
+	depth: any //深度,
+	detailType: any //病害小类
+	diseaseDate: any //病害日期
+	diseaseDiscription: string //病害描述
+	diseaseNo: string //病害编号
+	diseaseType: any //病害类型
+	dislocation: any //错台量
+	jointOpen: any //张开量
+	length: any //长度
+	photo: any //照片
+	recorder: string //记录人
+	width: any //宽度
+	updateCnt: number //更新次数
+
+	constructor() {
+		super()
+		this.id = '';
+		this.area = null;
+		this.delFlg = null;
+		this.depth = null;
+		this.detailType = null;
+		this.diseaseDate = null;
+		this.diseaseDiscription = null;
+		this.diseaseNo = null;
+		this.diseaseType = null;
+		this.dislocation = null;
+		this.jointOpen = null;
+		this.length = null;
+		this.photo = null;
+		this.recorder = null;
+		this.width = null;
+		this.updateCnt = null;
+	}
+
+  static deserialize(obj) {}
+}
---
+++ app/models/FacilityInspSummary.ts
@@ -0,0 +1,39 @@
+import {JsonProperty, Serializable} from '../providers/JsonMapper';
+import {_baseClass} from './_baseClass';
+
+@Serializable()
+export class FacilityInspSummary extends _baseClass { // 结构巡检明细表
+	diseaseNo: string //病害编号
+	delFlg: any //删除标记
+	diseaseDate: any //病害日期
+	facilityType: any // 设施小类
+	id: any //id
+	mfacilityList: any //设施编号
+	mileage: any //里程
+	modelNameList: any //模型名称
+	monomerNoList: any //单体名称
+	photoStandard: any //标准图
+	posDespList: any //位置描述
+	tagId: any //标签id
+	updateCnt: any //更新次数
+
+
+	constructor() {
+		super()
+		this.id = '';
+		this.diseaseNo = null;
+		this.delFlg = null;
+		this.diseaseDate = null;
+		this.facilityType = null;
+		this.mfacilityList = null;
+		this.mileage = null;
+		this.modelNameList = null;
+		this.monomerNoList = null;
+		this.photoStandard = null;
+		this.posDespList = null;
+		this.tagId = null;
+		this.updateCnt = null;
+	}
+
+  static deserialize(obj) {}
+}
