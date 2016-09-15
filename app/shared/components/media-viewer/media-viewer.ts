536520570f6c3fbf8b68ededc3f597210d0adc8a
GeorgeLin <georgelin@GeorgeLindeiMac.local>
2016-09-15 22:28:25 +0800

    save

--- app/shared/components/media-viewer/media-viewer.ts
+++ app/shared/components/media-viewer/media-viewer.ts
@@ -15,14 +15,9 @@ declare const Media: any;
 })
 export class MediaViewer{
   @Input() mediaContent: MediaContent;
-  @Input() imgOnly: boolean = false;
 
   @HostListener('click')
   eleClicked($event){
-    if(this.imgOnly){
-      this.viewImg(this.mediaContent);
-      return;
-    }
     switch(this.mediaContent.mediaType){
       case 'img':
         this.viewImg(this.mediaContent);
