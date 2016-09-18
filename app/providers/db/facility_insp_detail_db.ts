092f1723e714361e5f3f20708d0798ad7dc1d63c
jeremy_shao <jeremy.shao@pwc.com>
2016-09-18 23:37:36 +0800

    commit

--- app/providers/db/facility_insp_detail_db.ts
+++ app/providers/db/facility_insp_detail_db.ts
@@ -63,16 +63,12 @@ export class FacilityInspDetailDB {
   //获取巡检活动明细列表
   getAllFacilityInspDetails() {
     this._db = new PouchDB('facilityInspDetails', { adapter: 'websql', location: 'default' });
-  	if(!this._facilityInspDetails) {
-	  	return this._db.allDocs({include_docs: true})
-				.then(docs => {
-					this._facilityInspDetails = docs.rows.map(row => {
-						return row.doc
-					});
-					return this._facilityInspDetails;
-				})
-  	} else {
-  		return Promise.resolve(this._facilityInspDetails);
-  	}
+  	return this._db.allDocs({include_docs: true})
+			.then(docs => {
+				this._facilityInspDetails = docs.rows.map(row => {
+					return row.doc
+				});
+				return this._facilityInspDetails;
+			})
   }
 }
