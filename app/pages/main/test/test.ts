ee8e8f73aea8fd458713702738a497278ad6875e
GeorgeLin <georgelin@GeorgeLindeiMac.local>
2016-10-24 23:42:19 +0800

    save

--- app/pages/main/test/test.ts
+++ app/pages/main/test/test.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 
 declare const navigator;
+declare const MediaStreamTrack;
 
 @Component({
     template: `
@@ -26,26 +27,28 @@ export class TestPage implements OnInit {
         var videoElement = document.querySelector('video');
         var audioSelect = document.querySelector('select#audioSource');
         var videoSelect = document.querySelector('select#videoSource');
+        var cameras = [];
 
         navigator.getUserMedia = navigator.getUserMedia ||
         navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
 
         function gotSources(sourceInfos) {
-        for (var i = 0; i !== sourceInfos.length; ++i) {
-            var sourceInfo = sourceInfos[i];
-            var option = document.createElement('option');
-            option.value = sourceInfo.id;
-            if (sourceInfo.kind === 'audio') {
-            option.text = sourceInfo.label || 'microphone ' +
-                (audioSelect['length'] + 1);
-            audioSelect.appendChild(option);
-            } else if (sourceInfo.kind === 'video') {
-            option.text = sourceInfo.label || 'camera ' + (videoSelect['length'] + 1);
-            videoSelect.appendChild(option);
-            } else {
-            console.log('Some other kind of source: ', sourceInfo);
+            for (var i = 0; i !== sourceInfos.length; ++i) {
+                var sourceInfo = sourceInfos[i];
+                var option = document.createElement('option');
+                option.value = sourceInfo.id;
+                if (sourceInfo.kind === 'audio') {
+                    option.text = sourceInfo.label || 'microphone ' +
+                        (audioSelect['length'] + 1);
+                    audioSelect.appendChild(option);
+                } else if (sourceInfo.kind === 'video') {
+                    option.text = sourceInfo.label || 'camera ' + (videoSelect['length'] + 1);
+                    cameras.push(option.value);
+                    videoSelect.appendChild(option);
+                } else {
+                    console.log('Some other kind of source: ', sourceInfo);
+                }
             }
-        }
         }
 
         if (typeof MediaStreamTrack === 'undefined' ||
@@ -57,8 +60,8 @@ export class TestPage implements OnInit {
 
         function successCallback(stream) {
             window['stream'] = stream; // make stream available to console
-            videoElement.src = window.URL.createObjectURL(stream);
-            videoElement.play();
+            videoElement['src'] = window.URL.createObjectURL(stream);
+            videoElement['play']();
         }
 
         function errorCallback(error) {
@@ -66,30 +69,30 @@ export class TestPage implements OnInit {
         }
 
         function start() {
-        if (window['stream']) {
-            videoElement.src = null;
-            window['stream'].stop();
-        }
-        var audioSource = audioSelect['value'];
-        var videoSource = videoSelect['value'];
-        var constraints = {
-            audio: {
-            optional: [{
-                sourceId: audioSource
-            }]
-            },
-            video: {
-            optional: [{
-                sourceId: videoSource
-            }]
+            if (window['stream']) {
+                videoElement['src'] = null;
+                window['stream'].stop();
             }
-        };
-        navigator.getUserMedia(constraints, successCallback, errorCallback);
+            var audioSource = audioSelect['value'];
+            var videoSource = videoSelect['value'];
+            var constraints = {
+                audio: {
+                optional: [{
+                    sourceId: audioSource
+                }]
+                },
+                video: {
+                    optional: [{
+                        sourceId: cameras[1]
+                    }]
+                }
+            };
+            navigator.getUserMedia(constraints, successCallback, errorCallback);
         }
 
         audioSelect['onchange'] = start;
         videoSelect['onchange'] = start;
 
-        start();
+        // start();
     }
 }
